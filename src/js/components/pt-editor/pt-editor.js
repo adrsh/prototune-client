45a6191bb1e2a00f35acda151678d016b0dffad0
Adrian Shosholli <as227cw@student.lnu.se>
2022-04-24 03:57:09 +0200

    Add ability to send instruments to Websocket server, and partially be able to change instruments

--- src/js/components/pt-editor/pt-editor.js
+++ src/js/components/pt-editor/pt-editor.js
@@ -5,6 +5,8 @@
  * @version 1.0.0
  */
 
+ import * as Tone from 'tone'
+
 const template = document.createElement('template')
 template.innerHTML = `
   <style>
@@ -60,6 +62,85 @@ customElements.define('pt-editor',
       this.list = this.shadowRoot.querySelector('#list')
       this.rollHolder = this.shadowRoot.querySelector('#roll')
       this.button = this.shadowRoot.querySelector('button')
+
+      this.ws = window.ws
+
+      this.ws.addEventListener('open', () => {
+        this.ws.addEventListener('message', async event => {
+          const message = await event.message
+          this.#handleMessage(message)
+        })
+      })
+
+      this.config = {
+        attributes: true,
+        childList: true,
+        subtree: true,
+        attributeFilter: ['instrument']
+      }
+
+      this.observer = new MutationObserver(records => this.#handleMutations(records))
+      this.observer.observe(this.list, this.config)
+    }
+
+    /**
+     * Handles mutation records and sends appropriate messages.
+     *
+     * @param {MutationRecord[]} mutationRecords Mutation records.
+     */
+     #handleMutations (mutationRecords) {
+      const target = {}
+      for (const mutation of mutationRecords) {
+        // Checking if multiple attributes were changed at the same time, and combining them.
+        if (mutation.type === 'attributes') {
+          target.uuid = mutation.target.uuid
+          target.props = {
+            [mutation.attributeName]: mutation.target.getAttribute(mutation.attributeName)
+          }
+        } else if (mutation.type === 'childList') {
+          if (mutation.addedNodes.length > 0) {
+            for (const node of mutation.addedNodes) {
+              this.#sendMessage({
+                action: 'instrument-create',
+                uuid: node.uuid,
+                props: {
+                  roll: node.roll.uuid,
+                  instrument: node.getAttribute('instrument')
+                }
+              })
+            }
+          } else if (mutation.removedNodes.length > 0) {
+            for (const node of mutation.removedNodes) {
+              this.#sendMessage({ action: 'instrument-remove', uuid: node.uuid })
+            }
+          }
+        }
+      }
+      if (Object.keys(target).length > 0) {
+        this.#sendMessage({ action: 'instrument-update', ...target })
+      }
+    }
+
+    /**
+     * Handles messages from Websocket server.
+     *
+     * @param {object} message Message to be handled.
+     */
+    #handleMessage (message) {
+       if (message.action === 'instrument-add') {
+         console.log('add instrument')
+       }
+     }
+
+    /**
+     * Sends data to Websocket server.
+     *
+     * @param {object} data Data to be sent.
+     */
+    #sendMessage (data) {
+      if (this.ws.readyState === this.ws.OPEN) {
+        this.ws.send(JSON.stringify(data))
+      }
     }
 
     /**
@@ -85,16 +166,19 @@ customElements.define('pt-editor',
       const instrument = document.createElement('pt-instrument')
       const roll = document.createElement('pt-piano-roll')
 
-      roll.uuid = crypto.randomUUID()
-
       instrument.roll = roll
-      roll.instrument = instrument.instrument
+
+      instrument.setAttribute('instrument', 'casio')
+
+      instrument.uuid = crypto.randomUUID()
+      roll.uuid = crypto.randomUUID()
 
       instrument.addEventListener('instrument-select', event => {
         this.instrument = event.target.instrument
         this.roll = event.target.roll
         this.rollHolder.replaceChildren(event.target.roll)
       })
+
       this.list.insertBefore(instrument, this.button)
 
       this.rollHolder.replaceChildren(roll)
--- src/js/components/pt-instrument/pt-instrument.js
+++ src/js/components/pt-instrument/pt-instrument.js
@@ -32,45 +32,6 @@ customElements.define('pt-instrument',
       this.attachShadow({ mode: 'open' })
       this.shadowRoot.appendChild(template.content.cloneNode(true))
 
-      this.instrument = new Tone.Sampler({
-        urls: {
-          A0: 'A0.mp3',
-          C1: 'C1.mp3',
-          'D#1': 'Ds1.mp3',
-          'F#1': 'Fs1.mp3',
-          A1: 'A1.mp3',
-          C2: 'C2.mp3',
-          'D#2': 'Ds2.mp3',
-          'F#2': 'Fs2.mp3',
-          A2: 'A2.mp3',
-          C3: 'C3.mp3',
-          'D#3': 'Ds3.mp3',
-          'F#3': 'Fs3.mp3',
-          A3: 'A3.mp3',
-          C4: 'C4.mp3',
-          'D#4': 'Ds4.mp3',
-          'F#4': 'Fs4.mp3',
-          A4: 'A4.mp3',
-          C5: 'C5.mp3',
-          'D#5': 'Ds5.mp3',
-          'F#5': 'Fs5.mp3',
-          A5: 'A5.mp3',
-          C6: 'C6.mp3',
-          'D#6': 'Ds6.mp3',
-          'F#6': 'Fs6.mp3',
-          A6: 'A6.mp3',
-          C7: 'C7.mp3',
-          'D#7': 'Ds7.mp3',
-          'F#7': 'Fs7.mp3',
-          A7: 'A7.mp3',
-          C8: 'C8.mp3'
-        },
-        release: 1,
-        baseUrl: 'https://tonejs.github.io/audio/salamander/'
-      }).toDestination()
-
-      this.instrument.volume.value = -6
-
       this.name = this.shadowRoot.querySelector('#name')
     }
 
@@ -79,7 +40,6 @@ customElements.define('pt-instrument',
      */
     connectedCallback () {
       this.dispatchEvent(new CustomEvent('instrument-select'))
-      this.name.textContent = this.instrument.name
 
       this.addEventListener('click', () => this.dispatchEvent(new CustomEvent('instrument-select')))
     }
@@ -88,6 +48,104 @@ customElements.define('pt-instrument',
      * Called after the element is removed from the DOM.
      */
     disconnectedCallback () {
+      this.roll.remove()
+    }
+
+    /**
+     * Returns element attributes to observe.
+     *
+     * @returns {string[]} An array of attributes to observe.
+     */
+    static get observedAttributes () {
+      return ['instrument']
+    }
+
+    /**
+     * Called by the browser engine when an attribute changes.
+     *
+     * @param {string} name of the attribute.
+     * @param {any} oldValue the old attribute value.
+     * @param {any} newValue the new attribute value.
+     */
+    attributeChangedCallback (name, oldValue, newValue) {
+      if (name === 'instrument') {
+        this.#setInstrument(newValue)
+        this.roll.instrument = this.instrument
+        this.name.textContent = newValue
+      }
+    }
+
+    /**
+     * Changes instrument.
+     *
+     * @param {string} instrument Instrument to be played.
+     */
+    #setInstrument (instrument) {
+      if (this.instrument) {
+        this.instrument.dispose()
+      }
+      if (instrument === 'casio') {
+        this.instrument = new Tone.Sampler({
+          urls: {
+            A3: 'A1.mp3',
+            'A#3': 'As1.mp3',
+            B3: 'B1.mp3',
+            'G#3': 'Gs1.mp3',
+            A4: 'A2.mp3',
+            C4: 'C2.mp3',
+            'C#4': 'Cs2.mp3',
+            D4: 'D2.mp3',
+            'D#4': 'Ds2.mp3',
+            E4: 'E2.mp3',
+            F4: 'F2.mp3',
+            'F#4': 'Fs2.mp3',
+            G4: 'G2.mp3'
+          },
+          release: 1,
+          baseUrl: 'https://tonejs.github.io/audio/casio/'
+        }).toDestination()
+      } else if (instrument === 'piano') {
+        this.instrument = new Tone.Sampler({
+          urls: {
+            A0: 'A0.mp3',
+            C1: 'C1.mp3',
+            'D#1': 'Ds1.mp3',
+            'F#1': 'Fs1.mp3',
+            A1: 'A1.mp3',
+            C2: 'C2.mp3',
+            'D#2': 'Ds2.mp3',
+            'F#2': 'Fs2.mp3',
+            A2: 'A2.mp3',
+            C3: 'C3.mp3',
+            'D#3': 'Ds3.mp3',
+            'F#3': 'Fs3.mp3',
+            A3: 'A3.mp3',
+            C4: 'C4.mp3',
+            'D#4': 'Ds4.mp3',
+            'F#4': 'Fs4.mp3',
+            A4: 'A4.mp3',
+            C5: 'C5.mp3',
+            'D#5': 'Ds5.mp3',
+            'F#5': 'Fs5.mp3',
+            A5: 'A5.mp3',
+            C6: 'C6.mp3',
+            'D#6': 'Ds6.mp3',
+            'F#6': 'Fs6.mp3',
+            A6: 'A6.mp3',
+            C7: 'C7.mp3',
+            'D#7': 'Ds7.mp3',
+            'F#7': 'Fs7.mp3',
+            A7: 'A7.mp3',
+            C8: 'C8.mp3'
+          },
+          release: 1,
+          baseUrl: 'https://tonejs.github.io/audio/salamander/'
+        }).toDestination()
+      } else if (instrument === 'amsynth') {
+        this.instrument = new Tone.PolySynth(Tone.AMSynth).toDestination()
+      } else if (instrument === 'fmsynth') {
+        this.instrument = new Tone.PolySynth(Tone.FMSynth).toDestination()
+      }
     }
   }
 )

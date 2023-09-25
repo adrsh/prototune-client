68589872d6cfb32290db221c329b74b6f16d9ed7
Adrian Shosholli <as227cw@student.lnu.se>
2023-09-26 01:17:40 +0200

    Update dependencies and vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-export default {
+import { defineConfig } from 'vite'
+
+export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
     target: 'esnext'
+  },
+  server: {
+    port: 3000
   }
-}
+})
